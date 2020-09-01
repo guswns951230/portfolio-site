a0859ece48c97ae5c69700db7e6e070232649188
강현준 <kanghj@ganghyeonjun-ui-MacBookPro.local>
2020-09-01 18:49:55 +0900

    Final touch update

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 
 const header = document.querySelector('#header');
 const headerHeight = header.getBoundingClientRect().height;
-window.addEventListener('scroll', () => {
-	// console.log(window.scrollY);
-	// console.log(`headerHeight: ${headerHeight}`);
+document.addEventListener('scroll', () => {
+    // console.log(window.scrollY);
+    // console.log(`headerHeight: ${headerHeight}`);
     /* 백틱(')을 이용하여 원하는 값만 확인 가능 */
-    if(window.scrollY > headerHeight){
+    if (window.scrollY > headerHeight) {
         header.classList.add('header--dark');
-    } else{
+    } else {
         header.classList.remove('header--dark');
     }
 });
@@ -20,9 +20,10 @@ window.addEventListener('scroll', () => {
 
 const headerMenu = document.querySelector('.header__menu');
 headerMenu.addEventListener('click', (event) => {
+    console.log(event)
     const target = event.target;
     const link = target.dataset.link;
-    if(link==null){
+    if (link == null) {
         return;
     }
     headerMenu.classList.remove('open');
@@ -40,8 +41,9 @@ headerToggleBotton.addEventListener('click', () => {
 //2. contact section으로 스크롤 위치 변경
 
 const homecontactBtn = document.querySelector('.home__contact');
-homecontactBtn.addEventListener('click', ()=>{
+homecontactBtn.addEventListener('click', () => {
     scrollIntoViews('#contact');
+    headerMenu.classList.remove('open');
 });
 
 //scrolling이 될수록 home이 점점 투명해짐
@@ -51,8 +53,8 @@ const homeHeight = home.getBoundingClientRect().height;
 window.addEventListener('scroll', () => {
     // console.log(`homeHeight: ${homeHeight}`);
     // console.log(1 - window.scrollY/homeHeight);
-    home.style.opacity = 1 - window.scrollY/homeHeight;
-    
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+
 });
 
 // Arrow up button
@@ -60,9 +62,9 @@ window.addEventListener('scroll', () => {
 // 어디서든 이 버튼을 누르면 가장 위로 올라감
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', () => {
-    if(window.scrollY > homeHeight/2){
+    if (window.scrollY > homeHeight / 2) {
         arrowUp.classList.add('visible');
-    }else {
+    } else {
         arrowUp.classList.remove('visible');
     }
 });
@@ -71,14 +73,15 @@ arrowUp.addEventListener('click', () => {
     scrollIntoViews('#home');
 });
 
-//My work category를 누르면 해당 카테고리의 item만 보이게
-//html data를 이용
+// My work category를 누르면 해당 카테고리의 item만 보이게
+// html data를 이용
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.work__project');
 workBtnContainer.addEventListener('click', (e) => {
+    // console.log(e.target.dataset.filter);
     const filter = e.target.dataset.filter; // || e.target.parentNode.dataset.filter; 만약 filter가 없다면 부모노드의 dataset filter값을 받아 오겟다.work__projects
-    if(filter == null){
+    if (filter == null) {
         return; //filter가 null이라면 아무 행동도 취하지 않겟다.
     }
     //클릭한 버튼이 표시가 되게
@@ -90,23 +93,42 @@ workBtnContainer.addEventListener('click', (e) => {
     setTimeout(() => {
         projects.forEach((work__project) => {
             // console.log(work__project.dataset.type);
-            if(filter ==='*' || filter === work__project.dataset.type){
+            if (filter === '*' || filter === work__project.dataset.type) {
                 work__project.classList.remove('invisible');
-            }else{
+            } else {
                 work__project.classList.add('invisible');
             }
         });
         projectContainer.classList.remove('anim-out');
     }, 300)
+
 });
 
-
-
-
-
-
 // scrollIntoview()
-function scrollIntoViews(selector){ //selector만 일치하면 이동할 수 있게 함수를 지정
+function scrollIntoViews(selector) { //selector만 일치하면 이동할 수 있게 함수를 지정
     const scrollTo = document.querySelector(selector); //selector의 요소를 찾는다.
-    scrollTo.scrollIntoView({behavior: 'smooth'}); // 'smooth'하게 이동
+    scrollTo.scrollIntoView({ behavior: 'smooth' }); // 'smooth'하게 이동
 }
+
+
+
+//버튼을 누르면 해당되는 project만 나온다.
+//1.버튼을 가져온다
+//2.projects를 가져온다.
+
+// const categoryBtns = document.querySelector('.work__categories');
+// const categoryContainer = document.querySelector('.work__projects');
+// const projects = document.querySelectorAll('.work__project');
+// categoryBtns.addEventListener('click', (e) => {
+//     const dataFilter = e.target.dataset.filter;
+//     projects.forEach((work__project ) => {
+//         if(dataFilter === '*' || dataFilter === work__project.dataset.type){
+//             work__project.classList.remove('invisible')
+//         }else {
+//             work__project.classList.add("invisible")
+//         }
+//     });
+// });
+
+
+// console.log(projects);
